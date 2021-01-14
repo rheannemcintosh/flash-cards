629853d6cec85af3f1e1bb34cf0bcf38f0667d47
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-14 23:11:32 +0000

    Getting started with express

---
+++ app.js
@@ -0,0 +1,18 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>I love treehouse!</h1>');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello, JavaScript Developer</h1>');
+});
+
+
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+});
