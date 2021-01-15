0b670bdd3aaccf7e5e9b99e648a3e66228214da3
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-15 23:39:25 +0000

    Add form

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 const colours = [
     'red',
@@ -15,6 +20,14 @@ app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/hello', (req, res) => {
+    res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+    res.render('hello', { name: req.body.name });
 });
 
 app.get('/cards', (req, res) => {
