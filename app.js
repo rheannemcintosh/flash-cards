1d8fe7584e9ac39650565bdf68a61053d2418257
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-15 22:45:33 +0000

    Using templates with Express

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const express = require('express');
 
 const app = express();
 
+const colours = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+];
+
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello, JavaScript Developer</h1>');
+app.get('/cards', (req, res) => {
+    res.render('card', { prompt: "Who is buried in Grant's tomb?", hint:"Think about whose tomb it is", colours });
 });
-
-
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000');
